f9b55b768211e6e1e6f30048fd6f388b639daa2a
Maxwell <maxnovais@gmail.com>
2016-08-22 17:28:47 -0300

    Resolve views

--- uploader.py
+++ uploader.py
@@ -3,13 +3,14 @@ import os
 import re
 from unicodedata import normalize
 from uuid import uuid4
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 
 
 class Config(object):
     UPLOAD_FOLDER = '/tmp/flask_uploader/uploads'
     ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'html', 'py'])
     DEBUG = True
+    ITENS_PER_PAGE = 5
 
 
 app = Flask(__name__)
@@ -37,6 +38,7 @@ def secure_filename(filename):
 
 def resolves_dir(directory):
     try:
+
         os.mkdir(directory)
     except OSError:
         # When directory already exists, should return it
@@ -71,7 +73,34 @@ def send_file(file):
     return file_path
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+def home():
+    dir_list = [f for f in os.listdir(app.config['UPLOAD_FOLDER'])]
+    offset = int(request.args.get('offset')) if request.args.get('offset') else 0
+    limit = request.args.get('limit')
+
+    if not limit:
+        limit = offset + app.config['ITENS_PER_PAGE']
+
+    page = dir_list[offset:limit]
+
+    return render_template('directories.html', dirs=page, total_dir=len(dir_list), offset=offset, limit=limit)
+
+
+@app.route('/dir/<dir>', methods=['GET'])
+def get_dir(dir):
+    directory = os.path.join(app.config['UPLOAD_FOLDER'], dir)
+    file_list = [f for f in os.listdir(directory)]
+    return render_template('files.html', dir=dir, files=file_list, total_files=len(file_list))
+
+
+@app.route('/dir/<dir>/file/<file>', methods=['GET'])
+def get_file(dir, file):
+    directory = os.path.join(app.config['UPLOAD_FOLDER'], dir)
+    return send_from_directory(directory, filename=file)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         try:
